f9f058299945cb4685116c9bcb791515d2d08b98
Marcus Creese <marcuscreese94@gmail.com>
2022-02-11 09:11:55 -0600

    This is my first commit

---
+++ Pong/src/PongGame.java
@@ -0,0 +1,18 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+//1. Create a constructor for each class
+//2. Finish declaring the methods of each class
+//3. Instantiate everything we will need in PongGame class
+
+public class PongGame {
+
+	public static void main(String[] args) {
+	
+		GameFrame frame = new GameFrame();
+
+	}
+
+}
